bebff959c31f4538d2ec52fa65e819b0070adda5
Pedro Andrade <pedrosalvaro09@gmail.com>
2021-10-27 11:26:17 -0300

    confirmar

--- src/Pages/Inicial/index.js
+++ src/Pages/Inicial/index.js
@@ -1,12 +1,55 @@
 import React from "react";
-import './inicial.css'
+
 
 const Inicial = () => {
-    return(
-        <div className="inicial">
-            <h1>Inicial</h1>
+    return (
+     <div className="inicial">
+        <div class="header-1">
+        <div class="logo">
+            <img src="./img/logo.png" />
+        </div>
+        <div class="redes-sociais">
+             <ul>
+                <li>
+                    <a href=""><img src="./img/facebook.png" /></a>
+                </li>
+                 <li>
+                    <a href=""><img src="./img/tw.png" /></a>
+                </li>
+                <li>
+                    <a href=""><img src="./img/linkedin.png" /></a>
+                </li>
+                </ul>
+
+        <div class="col-100 menu-urls">
+        <div class="header-2">
+        <div class="menu">
+            <ul>
+                <li>
+                    <a href="">Home</a>
+                </li>
+                <li>
+                    <a href="">Login</a>
+                </li>
+                </ul>
+        <div class="busca">
+            <input
+                placeholder="Busque Aqui"
+                type="text"
+            />
+        <div className="footer">
+        <footer>
+        <p>Fale conosco:
+        <a href="mailto:hege@example.com">escoladeesteticaavpaulista@gmail.com</a></p>
+    </footer>
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>
         </div>
     );
 }
-
 export default Inicial;
