7912eba1f299350f63267188e94b229561fd6e4e
Pedro Andrade <pedrosalvaro09@gmail.com>
2021-10-27 13:06:44 -0300

    confirmar

--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -6,71 +6,71 @@ import { HiEye, HiEyeOff } from "react-icons/hi";
 import { useHistory } from "react-router-dom";
 
 function Login() {
-    const[email, setEmail] = useState("")
-    const[password, setPassword] = useState("")
-    const[show, setShow] = useState(false)
+const[email, setEmail] = useState("")
+const[password, setPassword] = useState("")
+const[show, setShow] = useState(false)
 
-    let history = useHistory("");
-
-
-    const handleClick = (e) => {
-        e.preventDefault()
-        setShow(!show);
-        history.push("/initial"); 
-    }
+let history = useHistory("");
 
 
-    return(
-        <div className="login">
-            <div className="login-logo">
-                <img src="https://www.gellog.com.br/assets/imagens/avatar.png" alt="Login-app" />
-            </div>
-            <div className="login-right">
-                <h1>Escola de Estética</h1>
+const handleClick = (e) => {
+e.preventDefault()
+setShow(!show);
+history.push("/initial"); 
+}
 
-                <div className="login-loginInputemail">
-                    <MdEmail />
-                    <input 
-                    type="email"
-                    placeholder="Digite seu Email"
-                    value={email}
-                    onChange={e => setEmail(e.target.value)} 
-                    />
-                </div>
-                <div className= "login-loginInputPassword">
-                    <MdLock />
-                    <input
-                        placeholder="Digite sua Senha"
-                        type={show ? "text" : "password"}
-                        value={password}
-                        onChange={e => setPassword(e.target.value)}
-                    />
-                    <div className="login-eye">
-                        {show ? (
-                            <HiEye 
-                                size={20}
-                                onClick={handleClick}
-                                />
-                        ) : (
-                            <HiEyeOff 
-                                size={20}
-                                onClick={handleClick}
-                            />
-                        )}
-                    </div>
-                </div>
-                <button type="submit" onClick={handleClick}>
-                Entrar
-                </button>
 
-                <h4>Não tenho uma conta</h4>
+return(
+<div className="login">
+<div className="login-logo">
+<img src="https://www.gellog.com.br/assets/imagens/avatar.png" alt="Login-app" />
+</div>
+<div className="login-right">
+<h1>Escola de Estética</h1>
 
-                <button type="submit">
-                    Cadastrar
-                </button>
-            </div>
-        </div>
-    );
+<div className="login-loginInputemail">
+    <MdEmail />
+    <input 
+    type="email"
+    placeholder="Digite seu Email"
+    value={email}
+    onChange={e => setEmail(e.target.value)} 
+    />
+</div>
+<div className= "login-loginInputPassword">
+    <MdLock />
+    <input
+        placeholder="Digite sua Senha"
+        type={show ? "text" : "password"}
+        value={password}
+        onChange={e => setPassword(e.target.value)}
+    />
+    <div className="login-eye">
+        {show ? (
+            <HiEye 
+                size={20}
+                onClick={handleClick}
+                />
+        ) : (
+            <HiEyeOff 
+                size={20}
+                onClick={handleClick}
+            />
+        )}
+    </div>
+</div>
+<button type="submit" onClick={handleClick}>
+Entrar
+</button>
+
+<h4>Não tenho uma conta</h4>
+
+<button type="submit">
+    Cadastrar
+</button>
+</div>
+</div>
+);
 }
 
 export default Login;
